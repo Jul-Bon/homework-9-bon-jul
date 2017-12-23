d2ff0afa552888a833873e825c23b3d073843d6f
Jul Bon <yulija.bon007@gmail.com>
2017-12-23 21:07:37 +0200

    sliders, popup...

---
+++ source-js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function () {
+  jQuery('#open-popup').on('click', function (event) {
+    jQuery('.popup').addClass('show')
+    event.preventDefault()
+  })
+  jQuery('#close-popup').on('click', function (event) {
+    jQuery('.popup').removeClass('show')
+    event.preventDefault()
+  })
+})
+
+jQuery(window).load(function (a) {
+  jQuery('.flexslider').flexslider({
+    animation: 'slide',
+    itemWidth: 360,
+    itemMargin: 30,
+    move: 1,
+    controlNav: false,
+    controlsContainer: jQuery('.our-members .custom-controls-container'),
+    customDirectionNav: jQuery('.our-members .custom-navigation a'),
+  })
+
+  jQuery('#videoslider').flexslider({
+    animation: 'slide',
+    animationLoop: true,
+    controlNav: false,
+    controlsContainer: jQuery('.videos .custom-controls-container'),
+    customDirectionNav: jQuery('.videos .custom-navigation a'),
+  })
+})
+
+
+
+
